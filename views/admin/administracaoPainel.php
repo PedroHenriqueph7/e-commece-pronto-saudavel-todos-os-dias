4be83814c727654d21c35bc7afb3935d75514e9a
Kenny <shadow.dio.shadow@gmail.com>
2025-11-30 23:35:24 -0300

    Página de Gestão/Administração
    Adicionada a página de gestão para a Glau

---
+++ views/admin/administracaoPainel.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel Gestor</title>
+    <link rel="stylesheet" href="/e-commece-pronto-saudavel-todos-os-dias/public/css/administracaoPainel.css">
+
+</head>
+<body>
+
+    <h1>Administrador/Gestor</h1>
+
+    <div class="administracao">
+
+        <a href="#" class="blocos">
+            <img src="https://cdn-icons-png.flaticon.com/512/3183/3183463.png" alt="Pedidos">
+            <span>Pedidos</span>
+        </a>
+
+        <a href="views/admin/listarProdutos.php" class="blocos">
+            <img src="https://cdn-icons-png.flaticon.com/512/3514/3514491.png" alt="Produtos">
+            <span>Produtos</span>
+        </a>
+
+    </div>
+
+</body>
+</html>
